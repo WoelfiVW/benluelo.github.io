1a640d911e5481a25ac381f834fbca9915932091
Ben <benluelo@hotmail.com>
2019-12-08 17:37:46 -0500

    i hate writing commit messages

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 // letter = document.getElementById("letter").addEventListener("submit", guesses = )
 
 function userGuessLetter() {
-    let userInput = document.getElementById("letter").value
+    let userInput = document.getElementById("letterGuess").value
     console.log("letter:", userInput)
 }
 
 function userGuessWord() {
-    let userInput = document.getElementById("word").value
+    let userInput = document.getElementById("wordGuess").value
     console.log("word:", userInput)
 }
 
 function userRestart() {
     console.log("restarting")
+    document.getElementById("start").style.display = "inline"
 }
 
 function userGiveUp() {
     console.log("user gave up")
+}
+
+function userStart() {
+    document.getElementById("start").style.display = "none"
+
+    let x = document.getElementsByClassName("init")
+    for(let i in x){
+        console.log(i, x[i])
+        x[i].style.display = "inline"
+    }
+
+    const word = lawnmower
 }
